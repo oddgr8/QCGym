b9f4ab876d8d057f3917588c133195d21ed4b761
oddgr8 <onkardeshpande07@gmail.com>
2021-02-15 21:25:36 +0530

    Add setup.cfg

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(name='QCGym', version='0.0.1', packages=['QCGym'], license='MIT',
       description='A Collection of Gym environments used in Quantum control',
       author='Onkar Deshpande', author_email='onkardeshpande07@gmail.com',
       url='https://github.com/oddgr8/QCGym',
-      download_url='',
+      download_url='https://github.com/oddgr8/QCGym/archive/v0.0.1-alpha.tar.gz',
       keywords=['Quantum', 'Control', 'Gym', 'RL'],
       # And any other dependencies foo needs,
-      install_requires=['gym', 'numpy']
+      install_requires=['gym', 'numpy'],
       classifiers=[
           # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
           'Development Status :: 3 - Alpha',
